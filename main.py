50010eb882ebc688c79053fe6b2071280de2aef8
kgpiannaman <namanbhatia.iitkgp@gmail.com>
2024-10-10 12:56:52 +0530

    editing json file with corrected format

--- main.py
+++ main.py
@@ -2,19 +2,26 @@
 
 import string
 #Read .txt file
-with open('textfile.txt', 'r', encoding='latin-1') as file:
-    content = file.read()
-    content = content.lower()
-#Initializing dictionary
-count_letter = {}
+# with open('textfile.txt', 'r', encoding='latin-1') as file:
+#     content = file.read()
+#     content = content.lower()
 
-#Finding number of occurences of 'l' in the content
-for letter in string.ascii_lowercase:
-    letter_count = content.count(letter)
-    count_letter[letter] = letter_count
-sorted_dict = dict(sorted(count_letter.items(),key=lambda item: item[1], reverse=True))
-print(sorted_dict)
-    # print("count of letter",letter,"is",letter_count)
+#Read json file
+import json
+
+# Open the JSON file with the correct encoding
+with open('textfile.json', 'r', encoding='utf-16') as file:
+    data = json.load(file)
+# #Initializing dictionary
+# count_letter = {}
+#
+# #Finding number of occurences of 'l' in the content
+# for letter in string.ascii_lowercase:
+#     letter_count = content.count(letter)
+#     count_letter[letter] = letter_count
+# sorted_dict = dict(sorted(count_letter.items(),key=lambda item: item[1], reverse=True))
+# print(sorted_dict)
+#     # print("count of letter",letter,"is",letter_count)
 
 
 
