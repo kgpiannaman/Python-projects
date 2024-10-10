276cdd8bdb8a8a0808ee697eeb8b7f2685572719
kgpiannaman <namanbhatia.iitkgp@gmail.com>
2024-10-10 14:54:52 +0530

    adding mcq json file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # This is a sample Python script.
-
 import string
 #Read .txt file
 # with open('textfile.txt', 'r', encoding='latin-1') as file:
@@ -11,17 +10,17 @@ import json
 
 # Open the JSON file with the correct encoding
 with open('textfile.json', 'r', encoding='utf-16') as file:
-    data = json.load(file)
+ content = str(json.load(file))
 # #Initializing dictionary
-# count_letter = {}
-#
-# #Finding number of occurences of 'l' in the content
-# for letter in string.ascii_lowercase:
-#     letter_count = content.count(letter)
-#     count_letter[letter] = letter_count
-# sorted_dict = dict(sorted(count_letter.items(),key=lambda item: item[1], reverse=True))
-# print(sorted_dict)
-#     # print("count of letter",letter,"is",letter_count)
+count_letter = {}
+
+ #Finding number of occurences of 'l' in the content
+for letter in string.ascii_lowercase:
+    letter_count = content.count(letter)
+    count_letter[letter] = letter_count
+sorted_dict = dict(sorted(count_letter.items(),key=lambda item: item[1], reverse=True))
+print(sorted_dict)
+
 
 
 
